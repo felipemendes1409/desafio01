c3bafd733ba713eb2232bd96f83b7b205e40fa31
Felipe <felipe.mds10@hotmail.com>
2023-02-19 14:34:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# desafio01-ruby
+ Desafio do Curso de Ruby da plataforma DIO, Ministrado pela instrutora Tenille, a proposta era criar um projeto que de a saida do nome completo e a idade.
---
+++ desafio01.rb
@@ -0,0 +1,16 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+print "Digite o ano de nascimento: "
+anoNascimento = gets.chomp.to_i
+print "Digite sua cidade: "
+cidade = gets.chomp
+print "Digite o Estado: "
+estado = gets.chomp
+
+puts "#{nome}, nasceu no ano de #{anoNascimento} e tem #{idade} anos, reside na cidade de #{cidade} no estado de #{estado}. "
+
+
+
+print "Olá #{nome}, Prazer em conhecê-lo!"
